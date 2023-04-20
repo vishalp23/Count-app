92f9a8fe96689e22de80d827f5b0939b9486caa7
killermonk <vishavvp230899@gmail.com>
2023-04-20 16:53:12 -0600

    Initial Comit

---
+++ src/components/navBar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              
+              
+            </span>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
